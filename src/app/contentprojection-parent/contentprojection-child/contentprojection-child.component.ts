9a6412cfc219dad4c38583814e730ff1a2a61c18
Sahil0607 <patel.sahil331@gmail.com>
2021-04-09 16:17:26 -0500

    content-projection

--- src/app/contentprojection-parent/contentprojection-child/contentprojection-child.component.ts
+++ src/app/contentprojection-parent/contentprojection-child/contentprojection-child.component.ts
@@ -6,12 +6,15 @@ import { Component, OnInit, Input } from '@angular/core';
   styleUrls: ['./contentprojection-child.component.css']
 })
 export class ContentprojectionChildComponent implements OnInit {
-  @Input() header: string;
-  today: Date = new Date();
+  @Input() header: String;
+  today = new Date().toDateString();
 
   constructor() { }
 
-  ngOnInit(): void {
-  }
-
+  ngOnInit(): void {}
+  
+  // Content Projection: Use when we need static panel and dynamics body in it.For proj. cont. <ng-content> in body
+  // If we have similar design in project but content will be dynamic in that purpose we will use content projection.
+  // If we have data such as string, number, arrey, obj we can pass with @Input() decorater, it will not use for html content.
+  // when we have html content then we need to use <ng-content> in child component's template.
 }
